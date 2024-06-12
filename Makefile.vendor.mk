4ebdd7a456413da7c66157ea36373f8265470d69
Filip Brychta <fbrychta@redhat.com>
2024-06-12 15:34:14 +0200

    OSSM-6655: creating product versions.yaml with only one version (#53)
    Signed-off-by: Filip Brychta <fbrychta@redhat.com>

--- Makefile.vendor.mk
+++ Makefile.vendor.mk
@@ -3,4 +3,4 @@ OPERATOR_NAME = servicemeshoperator3
 HUB = quay.io/maistra-dev
 CHANNELS = candidates
 HELM_VALUES_FILE = ossm/values.yaml
-VERSIONS_YAML_FILE = versions.yaml
+VERSIONS_YAML_FILE = ossm/versions.yaml
