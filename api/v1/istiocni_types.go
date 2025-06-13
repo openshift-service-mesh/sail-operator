6dfa01e7fa12207f845046149caa289c3bbb16e4
mkralik3 <mkralik@redhat.com>
2025-06-13 13:19:41 +0200

    Adding Istio 1.24.6

--- api/v1/istiocni_types.go
+++ api/v1/istiocni_types.go
@@ -28,10 +28,10 @@ const (
 type IstioCNISpec struct {
 	// +sail:version
 	// Defines the version of Istio to install.
-	// Must be one of: v1.24-latest, v1.24.5, v1.24.4, v1.24.3.
-	// +operator-sdk:csv:customresourcedefinitions:type=spec,order=1,displayName="Istio Version",xDescriptors={"urn:alm:descriptor:com.tectonic.ui:fieldGroup:General", "urn:alm:descriptor:com.tectonic.ui:select:v1.24-latest", "urn:alm:descriptor:com.tectonic.ui:select:v1.24.5", "urn:alm:descriptor:com.tectonic.ui:select:v1.24.4", "urn:alm:descriptor:com.tectonic.ui:select:v1.24.3"}
-	// +kubebuilder:validation:Enum=v1.24-latest;v1.24.5;v1.24.4;v1.24.3
-	// +kubebuilder:default=v1.24.5
+	// Must be one of: v1.24-latest, v1.24.6, v1.24.5, v1.24.4, v1.24.3.
+	// +operator-sdk:csv:customresourcedefinitions:type=spec,order=1,displayName="Istio Version",xDescriptors={"urn:alm:descriptor:com.tectonic.ui:fieldGroup:General", "urn:alm:descriptor:com.tectonic.ui:select:v1.24-latest", "urn:alm:descriptor:com.tectonic.ui:select:v1.24.6", "urn:alm:descriptor:com.tectonic.ui:select:v1.24.5", "urn:alm:descriptor:com.tectonic.ui:select:v1.24.4", "urn:alm:descriptor:com.tectonic.ui:select:v1.24.3"}
+	// +kubebuilder:validation:Enum=v1.24-latest;v1.24.6;v1.24.5;v1.24.4;v1.24.3
+	// +kubebuilder:default=v1.24.6
 	Version string `json:"version"`
 
 	// +sail:profile
@@ -178,7 +178,7 @@ type IstioCNI struct {
 	metav1.TypeMeta   `json:",inline"`
 	metav1.ObjectMeta `json:"metadata,omitempty"`
 
-	// +kubebuilder:default={version: "v1.24.5", namespace: "istio-cni"}
+	// +kubebuilder:default={version: "v1.24.6", namespace: "istio-cni"}
 	Spec IstioCNISpec `json:"spec,omitempty"`
 
 	Status IstioCNIStatus `json:"status,omitempty"`
