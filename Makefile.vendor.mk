1f3d993cbb53d1a37b60f23d6c1f1a719a00b4e8
Filip Brychta <fbrychta@redhat.com>
2025-10-23 12:06:22 +0200

    Do not patch the helm values (#569)
    This is required after https://github.com/istio-ecosystem/sail-operator/pull/1297
    
    (cherry picked from commit 234ba2326d12d62db31ddc42b487fd9288255f3f)

--- Makefile.vendor.mk
+++ Makefile.vendor.mk
@@ -6,7 +6,8 @@ DEFAULT_CHANNEL=stable
 HELM_VALUES_FILE = ossm/values.yaml
 VERSIONS_YAML_FILE ?= versions.ossm.yaml
 USE_IMAGE_DIGESTS = false
-GENERATE_RELATED_IMAGES = false
+PATCH_HELM_VALUES = false
+IMAGE ?= $$\{OSSM_OPERATOR_3_2\}
 
 .PHONY: build-fips
 build-fips: ## Build sail-operator binary for FIPS mode.
