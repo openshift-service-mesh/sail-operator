991747361e5044ab2f5b69415fe31fece292f1ed
bmangoen <bmangoen@redhat.com>
2026-01-20 09:10:04 +0100

    Adding 1.26.7 and 1.26.8 upstream helm charts and resources
    * Adding Istio 1.26.7 helm charts
    * Adding Istio 1.26.8 helm charts
    
    Signed-off-by: bmangoen <bmangoen@redhat.com>

--- api/v1alpha1/ztunnel_types.go
+++ api/v1alpha1/ztunnel_types.go
@@ -29,10 +29,10 @@ const (
 type ZTunnelSpec struct {
 	// +sail:version
 	// Defines the version of Istio to install.
-	// Must be one of: v1.26-latest, v1.26.6, v1.26.4, v1.26.3, v1.26.2, v1.24-latest, v1.24.6, v1.24.5, v1.24.4, v1.24.3.
-	// +operator-sdk:csv:customresourcedefinitions:type=spec,order=1,displayName="Istio Version",xDescriptors={"urn:alm:descriptor:com.tectonic.ui:fieldGroup:General", "urn:alm:descriptor:com.tectonic.ui:select:v1.26-latest", "urn:alm:descriptor:com.tectonic.ui:select:v1.26.6", "urn:alm:descriptor:com.tectonic.ui:select:v1.26.4", "urn:alm:descriptor:com.tectonic.ui:select:v1.26.3", "urn:alm:descriptor:com.tectonic.ui:select:v1.26.2", "urn:alm:descriptor:com.tectonic.ui:select:v1.24-latest", "urn:alm:descriptor:com.tectonic.ui:select:v1.24.6", "urn:alm:descriptor:com.tectonic.ui:select:v1.24.5", "urn:alm:descriptor:com.tectonic.ui:select:v1.24.4", "urn:alm:descriptor:com.tectonic.ui:select:v1.24.3"}
-	// +kubebuilder:validation:Enum=v1.26-latest;v1.26.6;v1.26.4;v1.26.3;v1.26.2;v1.24-latest;v1.24.6;v1.24.5;v1.24.4;v1.24.3
-	// +kubebuilder:default=v1.26.6
+	// Must be one of: v1.26-latest, v1.26.8, v1.26.7, v1.26.6, v1.26.4, v1.26.3, v1.26.2, v1.24-latest, v1.24.6, v1.24.5, v1.24.4, v1.24.3.
+	// +operator-sdk:csv:customresourcedefinitions:type=spec,order=1,displayName="Istio Version",xDescriptors={"urn:alm:descriptor:com.tectonic.ui:fieldGroup:General", "urn:alm:descriptor:com.tectonic.ui:select:v1.26-latest", "urn:alm:descriptor:com.tectonic.ui:select:v1.26.8", "urn:alm:descriptor:com.tectonic.ui:select:v1.26.7", "urn:alm:descriptor:com.tectonic.ui:select:v1.26.6", "urn:alm:descriptor:com.tectonic.ui:select:v1.26.4", "urn:alm:descriptor:com.tectonic.ui:select:v1.26.3", "urn:alm:descriptor:com.tectonic.ui:select:v1.26.2", "urn:alm:descriptor:com.tectonic.ui:select:v1.24-latest", "urn:alm:descriptor:com.tectonic.ui:select:v1.24.6", "urn:alm:descriptor:com.tectonic.ui:select:v1.24.5", "urn:alm:descriptor:com.tectonic.ui:select:v1.24.4", "urn:alm:descriptor:com.tectonic.ui:select:v1.24.3"}
+	// +kubebuilder:validation:Enum=v1.26-latest;v1.26.8;v1.26.7;v1.26.6;v1.26.4;v1.26.3;v1.26.2;v1.24-latest;v1.24.6;v1.24.5;v1.24.4;v1.24.3
+	// +kubebuilder:default=v1.26.8
 	Version string `json:"version"`
 
 	// +sail:profile
@@ -183,7 +183,7 @@ type ZTunnel struct {
 	// +optional
 	metav1.ObjectMeta `json:"metadata"`
 
-	// +kubebuilder:default={version: "v1.26.6", namespace: "ztunnel", profile: "ambient"}
+	// +kubebuilder:default={version: "v1.26.8", namespace: "ztunnel", profile: "ambient"}
 	// +optional
 	Spec ZTunnelSpec `json:"spec"`
 
